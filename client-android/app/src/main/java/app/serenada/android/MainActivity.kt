eb9f04dd36f90e5ae50ae80973af7ea472e1c5d9
nocom <nocomdev@bk.ru>
2026-02-11 13:49:35 +0400

    Revert "Fixed rotation bug while in a call joined from deep link"
    This reverts commit c771d050cddcc0f464b4d02a2d2cf07abd89c0f3.

--- client-android/app/src/main/java/app/serenada/android/MainActivity.kt
+++ client-android/app/src/main/java/app/serenada/android/MainActivity.kt
@@ -11,16 +11,12 @@ import androidx.compose.runtime.setValue
 import app.serenada.android.ui.SerenadaAppRoot
 
 class MainActivity : AppCompatActivity() {
-    companion object {
-        private const val STATE_PENDING_DEEP_LINK = "pending_deep_link"
-    }
-
     private val callManager by lazy { (application as SerenadaApp).callManager }
     private var pendingDeepLinkUri by mutableStateOf<Uri?>(null)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        pendingDeepLinkUri = restorePendingDeepLink(savedInstanceState)
+        pendingDeepLinkUri = intent?.data
         setContent {
             SerenadaAppRoot(
                 callManager = callManager,
@@ -32,22 +28,6 @@ class MainActivity : AppCompatActivity() {
 
     override fun onNewIntent(intent: Intent) {
         super.onNewIntent(intent)
-        setIntent(intent)
         pendingDeepLinkUri = intent.data
-    }
-
-    override fun onSaveInstanceState(outState: Bundle) {
-        super.onSaveInstanceState(outState)
-        outState.putString(STATE_PENDING_DEEP_LINK, pendingDeepLinkUri?.toString())
-    }
-
-    private fun restorePendingDeepLink(savedInstanceState: Bundle?): Uri? {
-        if (savedInstanceState?.containsKey(STATE_PENDING_DEEP_LINK) == true) {
-            return savedInstanceState
-                .getString(STATE_PENDING_DEEP_LINK)
-                ?.takeIf { it.isNotBlank() }
-                ?.let(Uri::parse)
-        }
-        return intent?.data
     }
 }
